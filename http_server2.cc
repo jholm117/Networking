a1d95cf495d884834b397f24a57bb161a9c2e987
LueptowK <kylelueptow@gmail.com>
2017-01-27 16:45:03 -0600

    updated server2.cc to use the old server1 code
    server1 did what server2 was supposed to do

--- http_server2.cc
+++ http_server2.cc
@@ -4,13 +4,16 @@
 
 #include "minet_socket.h"
 #include <stdlib.h>
-#include <fcntl.h>
 #include <ctype.h>
+#include <fcntl.h>
 #include <sys/stat.h>
 #include <iostream>
+#include <string>
 
 #define BUFSIZE 1024
 #define FILENAMESIZE 100
+
+using namespace std;
 
 int handle_connection(int);
 int writenbytes(int,char *,int);
@@ -21,14 +24,14 @@ int main(int argc,char *argv[])
   int server_port;
   int sock,sock2;
   struct sockaddr_in sa,sa2;
-  int rc,i;
-  fd_set readlist;
-  fd_set connections;
-  FD_ZERO(&readlist);
-  FD_ZERO(&connections);
-  int maxfd;
+  int rc;
   int addrlen;
-
+  fd_set read_fds;
+  fd_set master;
+  int fdmax;
+  FD_ZERO(&read_fds); //clear set
+  FD_ZERO(&master);
+  
   /* parse command line args */
   if (argc != 3)
   {
@@ -47,6 +50,7 @@ int main(int argc,char *argv[])
     perror("socket");
     exit(1);
   }
+  
 
   /* set server address*/
   sa.sin_family = AF_INET;
@@ -64,52 +68,52 @@ int main(int argc,char *argv[])
   if (listen(sock, 5) == -1) {
         perror("listen");
         exit(1);
-  }
-
-  maxfd = sock; // track max file descriptor
-
-  FD_ZERO(&connections);
-  FD_ZERO(&readlist);
-  FD_SET(sock, &readlist);
-
+    }
+  
+  FD_SET(sock, &master);  //add listening socket to set
+  fdmax = sock;
+  
   /* connection handling loop */
   while(1)
   {
-    /* create read list */
-    readlist = connections;
-
-    /* do a select */
-    if (select(maxfd+1, &readlist, NULL, NULL, NULL) == -1){
+    
+   
+    //wait until connection request has arrived
+    read_fds = master;
+    if(select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1) {
       perror("select");
       exit(1);
-    }
-
-    /* process sockets that are ready */
-    for(i = 0; i < maxfd; i++)
+    } 
+    for (int i = 0; i <= fdmax; i++)
     {
-      if(FD_ISSET(i, &readlist)){
-        /* for the accept socket, add accepted connection to connections */
-        if (i == sock)
+      if(FD_ISSET(i, &read_fds))
+      {
+        // sock ready to accept
+        if(i == sock)
         {
           addrlen = sizeof(sa2);
           if((sock2 = accept(sock, (struct sockaddr *) &sa2, (socklen_t *) &addrlen)) == -1)
             perror("accept");
           else
           {
-            FD_SET(sock2, &connections);
-            if(sock2 > maxfd)
-              maxfd = sock2;
-            //cout << "connection made" << endl;
+            FD_SET(sock2, &master); // add to master set
+            if (sock2 > fdmax)  // keep track of the maximum
+              fdmax = sock2;
+             cout << "connection made" << endl;
           }
         }
-        else /* for a connection socket, handle the connection */
+        else
         {
-  	     rc = handle_connection(i);
-         FD_CLR(i, &connections);
+          rc = handle_connection(i);
+          fdmax = sock;
         }
       }
-    }
+      
+      
+    } 
+  
   }
+    
 }
 
 int handle_connection(int sock2)
@@ -123,22 +127,22 @@ int handle_connection(int sock2)
   //char *endheaders;
   //char *bptr;
   //int datalen=0;
-  char *ok_response_f = "HTTP/1.0 200 OK\r\n"\
-                      "Content-type: text/plain\r\n"\
-                      "Content-length: %d \r\n\r\n";
-  char ok_response[100];
-  char *notok_response = "HTTP/1.0 404 FILE NOT FOUND\r\n"\
-                         "Content-type: text/html\r\n\r\n"\
-                         "<html><body bgColor=black text=white>\n"\
-                         "<h2>404 FILE NOT FOUND</h2>\n"\
-                         "</body></html>\n";
-  bool ok=true;
   char *filebuf;
   int fileSize;
+  char *ok_response_f = "HTTP/1.0 200 OK\r\n"\
+                      "Content-Type: text/plain\r\n"\
+                      "Content-Length: %d \r\n\r\n";
+  char ok_response[100];
+  char *notok_response = "HTTP/1.0 404 FILE NOT FOUND\r\n"\
+                         "Content-Type: text/html\r\n\r\n"\
+                         "<html><body bgColor=black text=white>\n"\
+                         "<h2>404 FILE NOT FOUND</h2>\n"
+                         "</body></html>\n";
+  bool ok=true;
   FILE * file;
   bzero(buf, BUFSIZE+1);
   bzero(filename, FILENAMESIZE+1);
-
+  
   /* first read loop -- get request and headers*/
   if(read(sock2, buf, BUFSIZE) < 0){
     perror("read");
@@ -164,34 +168,25 @@ int handle_connection(int sock2)
   /* send response */
   if (ok)
   {
-    
-    
     stat(filename, &filestat);
     fileSize = filestat.st_size;
     
     snprintf(ok_response, 100, ok_response_f,fileSize);
-      /* send headers */
+     /* send headers */
     
     int n = write(sock2, ok_response, strlen(ok_response)); 
     
-      /* send file */
-    
-    
-
+    /* send file */
     filebuf = new char[fileSize]; //filebuf is size of file
     
     fread(filebuf, sizeof(char), fileSize, file);   //extract file into ok_response
     
-    
-    n = write(sock2, filebuf, fileSize);          //write file contents
+    n = writenbytes(sock2, filebuf, fileSize);          //write file contents
     
     delete filebuf;
-      
   }
   else // send error response
-  {
     write(sock2,notok_response,strlen(notok_response));
-  }
 
   /* close socket and free space */
   close(sock2);
@@ -206,8 +201,9 @@ int readnbytes(int fd,char *buf,int size)
 {
   int rc = 0;
   int totalread = 0;
-  while ((rc = minet_read(fd,buf+totalread,size-totalread)) > 0)
+  while ((rc = read(fd,buf+totalread,size-totalread)) > 0)
     totalread += rc;
+
 
   if (rc < 0)
   {
@@ -221,7 +217,7 @@ int writenbytes(int fd,char *str,int size)
 {
   int rc = 0;
   int totalwritten =0;
-  while ((rc = minet_write(fd,str+totalwritten,size-totalwritten)) > 0)
+  while ((rc = write(fd,str+totalwritten,size-totalwritten)) > 0)
     totalwritten += rc;
 
   if (rc < 0)
@@ -229,4 +225,3 @@ int writenbytes(int fd,char *str,int size)
   else
     return totalwritten;
 }
-
